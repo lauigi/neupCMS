8539ff30e37c541bf5f8b5e37139cf2e1597fe84
frellica <gilbert_liu@qq.com>
2012-11-06 16:20:02 +0800

    fix a prob of url in articles

--- articles/urls.py
+++ articles/urls.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect
 urlpatterns = patterns('',
     url(r'^(?P<articleid>\d+)/$', 'articles.views.show_article'),
     url(r'^(?P<articleid>\d+)/edit/$', 'articles.views.edit_article'),
-    url(r'^?P<articleid>\d+)/verify/$', 'articles.views.verify_article'),
+    url(r'^(?P<articleid>\d+)/verify/$', 'articles.views.verify_article'),
     url(r'^(?P<articleid>\d+)/resume/$', 'articles.views.resume_article'),
     url(r'^(?P<articleid>\d+)/delete/$', 'articles.views.delete_article'),
     url(r'^new/$', 'articles.views.edit_article'),
